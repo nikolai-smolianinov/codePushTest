1300c6636c1f001e31d94e295d3623e82d619340
nikolay <it_5@beloris.ru>
2020-09-09 11:24:57 +0300

    testing

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, ScrollView, View, Button} from 'react-native';
+import codePush from "react-native-code-push";
 
 const App = () => {
+	const [value, setValue] = useState(1)
+
 	return (
 		<View style={styles.wrap}>
 			<Text style={styles.text}>
-				before code push
+				Value :
 			</Text>
+			<Text style={styles.hint}>no github</Text>
+
+			<Text style={styles.value}>
+				{value}
+			</Text>
+
+
+			<View style={{flexDirection: 'row', justifyContent: 'space-around', width: '100%', marginBottom: 25}}>
+				<Button  title="decrease" onPress={() => setValue(value - 1)} color="#f194ff"/>
+				<Button title="increase" onPress={() => setValue(value + 1)}/>
+			</View>
+				<Button
+					title="check For Update"
+					color="#48A868"
+					onPress={() => codePush.checkForUpdate().then(res => alert(JSON.stringify(res)))}
+				/>
+			<View style={styles.hr}/>
+				<Button
+					title="Restart app"
+					color="#48A868"
+					onPress={() => codePush.restartApp()}
+				/>
+			<View style={styles.hr}/>
+				{/*<Button*/}
+				{/*	title="clear Updates"*/}
+				{/*	color="#E8594C"*/}
+				{/*	onPress={() => codePush.clearUpdates()}*/}
+				{/*/>*/}
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
+	hr: {
+		height: 10
+	},
 	wrap: {
 		justifyContent: 'center',
 		alignItems: 'center',
@@ -21,6 +55,15 @@ const styles = StyleSheet.create({
 	text: {
 		fontSize: 42,
 		color: '#fafafa'
+	},
+	hint: {
+		marginTop: -10,
+		fontSize: 11,
+		color: '#fbfbfb'
+	},
+	value: {
+		fontSize: 36,
+		color: '#ffffff'
 	},
 });
 
--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import React from 'react'
+import {AppRegistry, View, Text} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import codePush from "react-native-code-push";
 
+class CodePushWrap extends React.Component {
 
-const WithCodePush = codePush(App);
+	constructor() {
+		super();
+		this.state = {
+			progress: null,
+			status: null
+		}
+	}
+
+	setStatus = status => {
+		this.setState({status})
+	}
+	setProgress = progress => {
+		this.setState({progress})
+	}
+	codePushStatusDidChange(status) {
+		switch(status) {
+			case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+				console.log("Checking for updates.");
+				break;
+			case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+				this.setStatus(status)
+				console.log("Downloading package.");
+				break;
+			case codePush.SyncStatus.INSTALLING_UPDATE:
+				console.log("Installing update.");
+				break;
+			case codePush.SyncStatus.UP_TO_DATE:
+				console.log("Up-to-date.");
+				break;
+			case codePush.SyncStatus.UPDATE_INSTALLED:
+				this.setStatus(status)
+				break;
+		}
+	}
+
+	codePushDownloadDidProgress(progress) {
+		this.setProgress(progress)
+
+		console.log(progress.receivedBytes + " of " + progress.totalBytes + " received.");
+	}
+
+	render() {
+
+		return (
+			<>
+				<App />
+
+
+				{
+					(!!this.state.status || !!this.state.progress) && (
+						<View style={{bottom: 0, backgroundColor: '#fff2f2', height: 50}}>
+							<Text>Status: {this.state.status}</Text>
+							<Text>Downloaded: {this.state.progress}</Text>
+						</View>
+					)
+				}
+
+			</>
+		)
+	}
+}
+
+
+const WithCodePush = codePush({ updateDialog: true, installMode: codePush.InstallMode.IMMEDIATE })(CodePushWrap);
 
 AppRegistry.registerComponent(appName, () => WithCodePush);
